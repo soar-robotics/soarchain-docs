2989470224600c96317e6f02eae947d29f2d159e
Hakan <hakanonize35@gmail.com>
2022-05-10 16:57:37 +0300

    Changed favicon and navbar icon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/sr.png',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -54,7 +54,7 @@ const config = {
         title: 'SoarChain',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          src: 'img/sr.png',
         },
         items: [
           {
@@ -64,8 +64,8 @@ const config = {
             label: 'Docs',
           },
           {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
+            href: 'https://github.com/soar-robotics/soarchain-docs',
+            label: 'GitHub - Coming Soon',
             position: 'right',
           },
         ],
